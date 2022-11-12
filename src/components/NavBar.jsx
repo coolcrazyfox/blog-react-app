8e279d06810e2a55433bb9d30c4d8fb9bbd7703a
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-12 17:53:03 +0300

    create NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import s from '../styles/NavBar.module.css'
 const NavBar = () => {
+    const [isActive, setIsActive]= useState(false)
+
+    const handleClick=(e)=>{
+        setIsActive(current=>!current)
+    }
     return (
-        <div>
-            
+        <div className={isActive? s.navigation_active: s.navigation}>
+            <div className={s.menuToggle} onClick={handleClick}></div>
+
         </div>
     );
 };
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from '../styles/Hero.module.css'
+import NavBar from "../components/NavBar";
 
 const Hero = () => {
     return (
         <div className={s.main_container}>
+            <NavBar/>
+            <div className={s.box}>
+
+            </div>
 
         </div>
     );

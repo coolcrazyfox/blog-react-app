240e7afed34552ec28df2c4ff3f6e415c8f77895
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-13 02:23:21 +0300

    changing NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,122 +30,125 @@ const NavBar = () => {
 
     }
     return (
-        <div className={isActive ? s.navigation_active : s.navigation}>
-            <div className={s.menuToggle} onClick={handleOnClick}></div>
-            {isActive ? (
-                    <ul>
-                        {tasks.map((t) => {
-                                return (
-                                    <li key={t.id} className={ s.list }
-                                        onClick={handleClick}
-                                    >
-                                    {/*<li key={t.id} className={check ? s.list : `${s.list} ${s.active}` }*/}
-                                    {/*    // onClick={handleClick}*/}
-                                    {/*>*/}
+        <div className={isActive? s.main_active: s.main}>
+            <div className={isActive ? s.navigation_active : s.navigation}>
+                <div className={s.menuToggle} onClick={handleOnClick}></div>
+                {isActive ? (
+                        <ul>
+                            {tasks.map((t) => {
+                                    return (
+                                        <li key={t.id} className={ s.list }
+                                            onClick={handleClick}
+                                        >
+                                            {/*<li key={t.id} className={check ? s.list : `${s.list} ${s.active}` }*/}
+                                            {/*    // onClick={handleClick}*/}
+                                            {/*>*/}
 
-                                        <Link  className={s.a} to={t.link} style={{color: `${t.color}`}}>
+                                            <Link  className={s.a} to={t.link} style={{color: `${t.color}`}}>
 
-                                            {t.id === 1 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}}   >
+                                                {t.id === 1 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}}   >
                                                     <SiHomeassistant/>
                                                 </span>
-                                            )}
-                                            {t.id === 2 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}} >
+                                                )}
+                                                {t.id === 2 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}} >
                                                     <SiMicrodotblog/>
                                                 </span>
-                                            )}
-                                            {t.id === 3 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}} >
+                                                )}
+                                                {t.id === 3 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}} >
                                                     <GiPostStamp/>
                                                 </span>
-                                            )}
-                                            {t.id === 4 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}} >
+                                                )}
+                                                {t.id === 4 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}} >
                                                     <SlLogin/>
                                                 </span>
-                                            )}
-                                            {t.id === 5 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}} >
+                                                )}
+                                                {t.id === 5 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}} >
                                                     <HiUserGroup/>
                                                 </span>
-                                            )}
-                                            {t.id === 6 && (
-                                                <span className={s.icon} style={{background: `${t.color}`}} >
+                                                )}
+                                                {t.id === 6 && (
+                                                    <span className={s.icon} style={{background: `${t.color}`}} >
                                                     <MdSettingsSuggest/>
                                                 </span>
-                                            )}
+                                                )}
 
 
-                                            <span className={s.text}>{t.title}</span>
-                                        </Link>
-                                    </li>
-                                )
+                                                <span className={s.text}>{t.title}</span>
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            )
                             }
-                        )
-                        }
 
 
-                    </ul>) :
-                (<ul>
-                    <ul>
-                        {tasks.map((t) => {
-                                return (
-                                    <li key={t.id} className={ s.list }
-                                        onClick={handleClick}
-                                    >
-                                        {/*<li key={t.id} className={check ? s.list : `${s.list} ${s.active}` }*/}
-                                        {/*    // onClick={handleClick}*/}
-                                        {/*>*/}
+                        </ul>) :
+                    (<ul>
+                        <ul>
+                            {tasks.map((t) => {
+                                    return (
+                                        <li key={t.id} className={ s.list }
+                                            onClick={handleClick}
+                                        >
+                                            {/*<li key={t.id} className={check ? s.list : `${s.list} ${s.active}` }*/}
+                                            {/*    // onClick={handleClick}*/}
+                                            {/*>*/}
 
-                                        <Link to={t.link} style={{color: `${t.color}`}}>
+                                            <Link to={t.link} style={{color: `${t.color}`}}>
 
-                                            {t.id === 1 && (
-                                                <span className={s.icon}>
+                                                {t.id === 1 && (
+                                                    <span className={s.icon}>
                                                     <SiHomeassistant/>
                                                 </span>
-                                            )}
-                                            {t.id === 2 && (
-                                                <span className={s.icon}>
+                                                )}
+                                                {t.id === 2 && (
+                                                    <span className={s.icon}>
                                                     <SiMicrodotblog/>
                                                 </span>
-                                            )}
-                                            {t.id === 3 && (
-                                                <span className={s.icon}>
+                                                )}
+                                                {t.id === 3 && (
+                                                    <span className={s.icon}>
                                                     <GiPostStamp/>
                                                 </span>
-                                            )}
-                                            {t.id === 4 && (
-                                                <span className={s.icon}>
+                                                )}
+                                                {t.id === 4 && (
+                                                    <span className={s.icon}>
                                                     <SlLogin/>
                                                 </span>
-                                            )}
-                                            {t.id === 5 && (
-                                                <span className={s.icon}>
+                                                )}
+                                                {t.id === 5 && (
+                                                    <span className={s.icon}>
                                                     <HiUserGroup/>
                                                 </span>
-                                            )}
-                                            {t.id === 6 && (
-                                                <span className={s.icon}>
+                                                )}
+                                                {t.id === 6 && (
+                                                    <span className={s.icon}>
                                                     <MdSettingsSuggest/>
                                                 </span>
-                                            )}
+                                                )}
 
 
-                                            <span className={s.text}>{t.title}</span>
-                                        </Link>
-                                    </li>
-                                )
+                                                <span className={s.text}>{t.title}</span>
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            )
                             }
-                        )
-                        }
 
 
-                    </ul>
+                        </ul>
 
-                </ul>)}
+                    </ul>)}
 
+            </div>
         </div>
+
     );
 };
 

dbc6146ead1d6b3f190cd8d2685aea247fc401b4
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-13 23:35:31 +0300

    changing component : Blogs

--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -3,8 +3,44 @@ import s  from '../styles/BlogsList.module.css'
 const BlogsList = () => {
     return (
         <div className={s.blogs}>
-            {/*blog*/}
-            <div className={s.box}>BlogsList</div>
+            <div className={s.box}>
+                <div className={s.container_items}>
+                    <div className={s.blog_item}>
+                        <div className={s.image_blog}>
+                            {/*<img src="https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg" alt="img"/>*/}
+                        </div>
+
+                        <div className={s.text_container}>
+                            <h2> The best blog in our village</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus et, eveniet odio porro quod rem sequi vel. Ad aut culpa deleniti eum ex illo iusto, odit provident repudiandae unde veniam!</p>
+                        </div>
+
+                    </div>
+                    <div className={s.blog_item}>
+                        <div className={s.image_blog}>
+                            {/*<img src="https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg" alt="img"/>*/}
+                        </div>
+
+                        <div className={s.text_container}>
+                            <h2> The best blog in our village</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus et, eveniet odio porro quod rem sequi vel. Ad aut culpa deleniti eum ex illo iusto, odit provident repudiandae unde veniam!</p>
+                        </div>
+
+                    </div>
+                    <div className={s.blog_item}>
+                        <div className={s.image_blog}>
+                            {/*<img src="https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg" alt="img"/>*/}
+                        </div>
+
+                        <div className={s.text_container}>
+                            <h2> The best blog in our village</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus et, eveniet odio porro quod rem sequi vel. Ad aut culpa deleniti eum ex illo iusto, odit provident repudiandae unde veniam!</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     );

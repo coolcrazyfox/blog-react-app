9d94f879e7b226628ddd2f6dd32c477f8cca2b65
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-13 22:34:54 +0300

    changing component : Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,19 @@ import s from '../styles/Search.module.css'
 const Search = () => {
     return (
         <div className={s.search}>
-            Search
+            <div className={s.title}> Blogs</div>
+            <div className={s.search_box}>
+                <input type="text"
+                placeholder={"Search"}/>
+                <select>
+                    <option selected value="New blogs first">New blogs first</option>
+                    <option value="New blogs last">New blogs last</option>
+                    <option value="Last blogs first">Last blogs first</option>
+                    <option value="Last blogs last">Last blogs last</option>
+                </select>
+
+            </div>
+
         </div>
     );
 };

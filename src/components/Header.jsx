3936035891d0243042d367e8507772b378d7b836
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-13 22:07:01 +0300

    changing component : header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import s from '../styles/Header.module.css'
 const Header = () => {
     return (
         <div className={s.header}>
-            <div className={s.title}>Title header </div>
+            <div className={s.title}>
+                <h4>Blogs Form</h4>
+            </div>
         </div>
     );
 };

fec2f8f07ccb3e82a54a0ccca34f08f55a0b31f2
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-17 23:55:10 +0300

    changing component : Search.jsx

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import s from "../styles/Posts.module.css";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Search from "../components/Search";
+import BlogsList from "../components/BlogsList";
+import Footer from "../components/Footer";
 
 const Posts = () => {
     return (
-        <div>
-            Posts
+        <div className={s.main_container}>
+            <Header/>
+            <NavBar/>
+            <Search/>
+            <BlogsList/>
+            <Footer/>
         </div>
     );
 };

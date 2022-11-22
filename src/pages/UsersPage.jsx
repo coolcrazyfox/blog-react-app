7e67929ff09307a1683fec5e12f356f6c61ac470
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-22 21:50:19 +0300

    changing  SupperButton.jsx

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import s from '../styles/Use'
 const UsersPage = () => {
+    const [active, setActive] = useState(0)
+    const title = ['blogs', 'posts']
+    const handleOnClick = (index) => {
+        setActive(index)
+
+    }
     return (
-        <div>
-            Users Page
+        <div className={s.main}>
+            <ul>
+                {title.map((t, i) => (
+                    <li
+                        className={active === i ? "active" : ''}
+                        onClick={() => handleOnClick(i)}
+                    >
+                        {t}
+                    </li>
+                ))}
+            </ul>
+
         </div>
     );
 };

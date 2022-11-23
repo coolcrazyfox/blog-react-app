6ea450d52d72303add0ac23f00b5a8743ab575b5
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-24 00:02:09 +0300

    change DetailPostBlock.module.css

--- src/components/DetailPostBlock.jsx
+++ src/components/DetailPostBlock.jsx
@@ -1,15 +1,35 @@
 import React, {useState} from 'react';
 import s from "../styles/DetailPostBlock.module.css";
-import {initialTasks} from "./Blog";
+import {initialPostsTasks} from "./Post";
+
 
 
 const DetailPostBlock = () => {
-    const [detail, setDetail] = useState(initialTasks)
-    const card = initialTasks[0]
+    const [detail, setDetail] = useState(initialPostsTasks)
+    const card = detail[0]
     return (
         <div className={s.blogs}>
             <div className={s.box}>
+                <div className={s.container}>
+                    <div className={s.post_info_box}>
+                        <img src={card.image} alt="img"/>
+                        <p>Back-end blog</p>
+                    </div>
+                    <div className={s.title}>
+                       <h2>
+                           First day at the office
+                       </h2>
+                    </div>
+                    <div className={s.date_info}>
+                        12/12/2022 at 15:46:58
+                    </div>
+                    <div className={s.main_img}>
+                        <img src={card.image}  alt="img"/>
+                    </div>
+                    <textarea className={s.text_form} name="" id="" cols="30" rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolorum quis repellendus sunt veritatis. Accusamus aperiam architecto blanditiis dolores harum impedit minima non quo saepe totam ullam, vel vero voluptatibus.
+                    </textarea>
 
+                </div>
             </div>
 
 

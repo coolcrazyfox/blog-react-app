6feaf097edf680a62cb16bd075089aa10170c5dd
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-24 23:06:12 +0300

    change NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,12 +9,12 @@ import {Link, NavLink} from "react-router-dom";
 
 
 export const initialState = [
-    {id: 1, link: '/', title: "Home", color: "#D73486FF", act: false, icon: SiHomeassistant},
-    {id: 2, link: '/blogger', title: "Blogs", color: "#cea602", act: false, icon: SiMicrodotblog},
-    {id: 3, link: '/posts', title: "Posts", color: "#864E4CFF", act: false, icon: SiApostrophe},
-    {id: 4, link: '/registration', title: "Log In", color: "#ec8434", act: false, icon: RiLoginCircleFill},
-    {id: 5, link: '/users', title: "Users", color: "#8BB92CFF", act: false, icon: HiUserGroup},
-    {id: 6, link: '/settings', title: "Settings", color: "#A61111FF", act: false, icon: MdSettingsSuggest}
+    {id: 1, link: '/', title: "Home", color: "#D73486FF", act: false, icon: <SiHomeassistant/>},
+    {id: 2, link: '/blogger', title: "Blogs", color: "#cea602", act: false, icon: <SiMicrodotblog/>},
+    {id: 3, link: '/posts', title: "Posts", color: "#864E4CFF", act: false, icon: <SiApostrophe/>},
+    {id: 4, link: '/registration', title: "Log In", color: "#ec8434", act: false, icon: <RiLoginCircleFill/>},
+    {id: 5, link: '/users', title: "Users", color: "#8BB92CFF", act: false, icon: <HiUserGroup/>},
+    {id: 6, link: '/settings', title: "Settings", color: "#A61111FF", act: false, icon: <MdSettingsSuggest/>}
 ]
 console.log('style',s)
 const NavBar = () => {
@@ -51,32 +51,36 @@ const NavBar = () => {
 
                                                 {t.id === 1 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}}   >
-                                                    <SiHomeassistant/>
+                                                    {/*<SiHomeassistant/>*/}{t.icon}
                                                 </span>
                                                 )}
                                                 {t.id === 2 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}} >
-                                                    <SiMicrodotblog/>
+                                                    {/*<SiMicrodotblog/>*/}
+                                                        {t.icon}
                                                 </span>
                                                 )}
                                                 {t.id === 3 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}} >
-                                                    <SiApostrophe/>
+                                                    {/*<SiApostrophe/>*/}
+                                                        {t.icon}
                                                 </span>
                                                 )}
                                                 {t.id === 4 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}} >
-                                                    <RiLoginCircleFill/>
+                                                    {/*<RiLoginCircleFill/>*/}
+                                                        {t.icon}
                                                 </span>
                                                 )}
                                                 {t.id === 5 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}} >
-                                                    <HiUserGroup/>
+                                                    {/*<HiUserGroup/>*/}
                                                 </span>
                                                 )}
                                                 {t.id === 6 && (
                                                     <span className={s.icon} style={{background: `${t.color}`}} >
-                                                    <MdSettingsSuggest/>
+                                                    {/*<MdSettingsSuggest/>*/}
+                                                        {t.icon}
                                                 </span>
                                                 )}
                                                 <span className={s.text}>{t.title}</span>

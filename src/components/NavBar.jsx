291099462ae1ba2d7cfcd98b3b018eb2699c8726
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-25 20:35:08 +0300

    change NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,14 +9,14 @@ import {Link} from "react-router-dom";
 
 
 export const initialState = [
-    {id: 1, link: '/', title: "Home", color: "#D73486FF", act: false, icon: <SiHomeassistant/>},
-    {id: 2, link: '/blogger', title: "Blogs", color: "#cea602", act: false, icon: <SiMicrodotblog/>},
-    {id: 3, link: '/posts', title: "Posts", color: "#864E4CFF", act: false, icon: <SiApostrophe/>},
-    {id: 4, link: '/registration', title: "Log In", color: "#ec8434", act: false, icon: <RiLoginCircleFill/>},
-    {id: 5, link: '/users', title: "Users", color: "#8BB92CFF", act: false, icon: <HiUserGroup/>},
-    {id: 6, link: '/settings', title: "Settings", color: "#A61111FF", act: false, icon: <MdSettingsSuggest/>}
+    {id: 1, link: '/', title: "Home", color: "#D73486FF", icon: <SiHomeassistant/>},
+    {id: 2, link: '/blogger', title: "Blogs", color: "#cea602",  icon: <SiMicrodotblog/>},
+    {id: 3, link: '/posts', title: "Posts", color: "#864E4CFF", icon: <SiApostrophe/>},
+    {id: 4, link: '/registration', title: "Log In", color: "#ec8434",  icon: <RiLoginCircleFill/>},
+    {id: 5, link: '/users', title: "Users", color: "#8BB92CFF",  icon: <HiUserGroup/>},
+    {id: 6, link: '/settings', title: "Settings", color: "#A61111FF", icon: <MdSettingsSuggest/>}
 ]
-console.log('style', s)
+
 const NavBar = () => {
     // const [tasks, setTasks] = useState(initialState)
     const [isActive, setIsActive] = useState(false)

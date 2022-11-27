a3789fdf504fcd81aebd30701e341fd75b400dce
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-27 21:31:37 +0300

    changing Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import s from '../styles/Menu.module.css'
 
-const Menu = ({header, items}) => {
+const Menu = ({header, items, active, setActive}) => {
     return (
-        <div className={s.menu}>
+        <div className={active ? `${s.menu}${s.active}`: s.menu}>
             <div className={s.blur}></div>
             <div className={s.menu_content}>
                 <div className={s.menu_header}>{header}</div>
                 <ul>
                     {items.map(item=>
                     <li>
-                        <Link>{item.value}</Link>
+                        <Link to={item.link}>{item.value}</Link>
                         <span className={s.icons}>{item.icon}</span>
                     </li>)}
                 </ul>
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import s from '../styles/UsersPage.module.css'
 import SupperButton from "../components/SupperButton";
 import NavBarList from "../components/NavBarList";
-import {SiAbbvie, SiAcer, SiApostrophe} from "react-icons/si";
+import {SiApostrophe,SiMicrodotblog, SiPostman} from "react-icons/si";
 import Menu from "../components/Menu";
 const UsersPage = () => {
     const items=[
-        {value:'Home', href:'/', icon:<SiApostrophe/>},
-        {value:'Blogs', href:'/blogs', icon:<SiAbbvie/>},
-        {value:'Posts', href:'/posts', icon:<SiAcer/>},
+        {value:'Home', link:'/', icon:<SiApostrophe/>},
+        {value:'Blogs', link:'/blogger', icon:<SiMicrodotblog/>},
+        {value:'Posts', link:'/posts', icon:<SiPostman/>},
 
     ]
-
+    const [menuActive,setMenuActive]=useState(false)
     const [active, setActive] = useState(0)
     const title = ['blogs', 'posts',' details']
     const handleOnClick = (index) => {
@@ -20,7 +20,11 @@ const UsersPage = () => {
     }
     return (
         <div className={s.mainn}>
-            <Menu items={items} header={'Menu'}/>
+            <nav>
+                <button className={s.main_btn} onClick={()=>setMenuActive(!menuActive)}>x</button>
+            </nav>
+
+            <Menu items={items} header={'Menu'}  active={menuActive} setActive={setMenuActive}/>
             {/*<NavBarList/>*/}
             {/*<ul>*/}
             {/*    {title.map((t, i) => (*/}

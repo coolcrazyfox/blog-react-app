f0a19ee73bf83680c7ea3425de590472bbb002cf
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-28 23:03:07 +0300

    create RegistrationPage.jsx

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import s from '../styles/RegistrationPage.module.css'
+import {SiHomeassistant} from "react-icons/si";
 
 const RegistrationPage = () => {
     return (
-        <div>
-            Registration Page
+        <div className={s.main_container}>
+
+
+            <div className={s.sidebar}>
+                <header>Menu</header>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i className={s.fas}>
+                                <SiHomeassistant/>
+                                Blogs
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className={s.fas}>
+                                <SiHomeassistant/>
+                                Posts
+                            </i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
 
         </div>
     );

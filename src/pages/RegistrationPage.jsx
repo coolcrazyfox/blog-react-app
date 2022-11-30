64a63fd047b8a7531cffa48425d315d767106597
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-30 21:52:46 +0300

    changing RegistrationPage.jsx

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import s from '../styles/RegistrationPage.module.css'
-import {SiHomeassistant} from "react-icons/si";
-import {FaBars} from "react-icons/fa";
-import {ImCancelCircle} from "react-icons/im";
+import {initialState} from "../components/NavBar";
+import {Link} from "react-router-dom";
 
 const RegistrationPage = () => {
-    const [isActive, setIsActive]=useState(false)
-    const handelClick=()=>{
+    const tasks = initialState
+    const [isActive, setIsActive] = useState(false)
+    const handelClick = () => {
         // setActive(current => !current)
         setIsActive(!isActive)
 
@@ -16,12 +16,24 @@ const RegistrationPage = () => {
         <div className={s.main_container}>
 
             {/*<div className={isActive? s.navigation_box: ''}>*/}
-                <div className={isActive? s.navigationn_active: s.navigationn}>
-                    <div className={s.menu_toggle} onClick={()=>setIsActive(!isActive)}></div>
+            <div className={isActive ? s.navigationn_active : s.navigationn}>
+                <div className={s.menu_toggle} onClick={() => setIsActive(!isActive)}>
+                    <ul>
+                        {tasks.map(t => {
+                                return (
+                                    <li key={t.id} className={s.lists}>
+                                        <Link to={t.link}>
+                                            <span className={s.icons}>{t.icon}</span>
+                                            <span className={s.title}>{t.title}</span>
+                                        </Link>
+
+                                    </li>
+                                )
+                            }
+                        )}
+                    </ul>
                 </div>
-
-
-
+            </div>
 
 
             {/*</div>*/}

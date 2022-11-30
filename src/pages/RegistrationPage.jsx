da115ba689f1a592ed1d5f260ecd3fe1cc7a5870
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-11-30 23:00:32 +0300

    changing RegistrationPage.css

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -6,6 +6,8 @@ import {Link} from "react-router-dom";
 const RegistrationPage = () => {
     const tasks = initialState
     const [isActive, setIsActive] = useState(false)
+    const [liActive, setLiActive] = useState(false)
+    const [activeId, setActiveId] = useState(1)
     const handelClick = () => {
         // setActive(current => !current)
         setIsActive(!isActive)
@@ -17,22 +19,21 @@ const RegistrationPage = () => {
 
             {/*<div className={isActive? s.navigation_box: ''}>*/}
             <div className={isActive ? s.navigationn_active : s.navigationn}>
-                <div className={s.menu_toggle} onClick={() => setIsActive(!isActive)}>
-                    <ul>
-                        {tasks.map(t => {
-                                return (
-                                    <li key={t.id} className={s.lists}>
-                                        <Link to={t.link}>
-                                            <span className={s.icons}>{t.icon}</span>
-                                            <span className={s.title}>{t.title}</span>
-                                        </Link>
+                <div className={s.menu_toggle} onClick={() => setIsActive(!isActive)}></div>
+                <ul>
+                    {tasks.map((t, id) => {
+                            return (
+                                <li key={t.id} className={ activeId===id? s.lists_active : s.lists} style={{color:`${t.color}`}} onClick={()=>setActiveId(id)}>
+                                    <Link to={t.link}>
+                                        <span className={s.icons}>{t.icon}</span>
+                                        <span className={s.title}>{t.title}</span>
+                                    </Link>
 
-                                    </li>
-                                )
-                            }
-                        )}
-                    </ul>
-                </div>
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
             </div>
 
 

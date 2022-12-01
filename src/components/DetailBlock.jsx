52014b0a23c9932a495dad6627f5da070f377f26
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-12-01 21:46:33 +0300

    changing DetailBlock.jsx

--- src/components/DetailBlock.jsx
+++ src/components/DetailBlock.jsx
@@ -6,8 +6,12 @@ import {initialTasks} from "./Blog";
 const DetailBlock = () => {
     const [isOpen, setIsOpen]= useState(false)
     const [selected, setSelected]=useState(0)
-    const [detail, setDetail] = useState(initialTasks)
-    const card = initialTasks[0]
+    const details = initialTasks
+    const sortDetails=details[selected]
+    const onClickListItem=(i)=>{
+        setSelected(i)
+
+    }
     return (
         <div className={s.blogs}>
             <div className={s.box}>
@@ -16,20 +20,20 @@ const DetailBlock = () => {
                         <div className={s.item_card}>
                             <div className={s.main_icon_box}>
                                 <div className={s.main_icon}
-                                     style={{backgroundImage:`url(${card.image})`}}>
+                                     style={{backgroundImage:`url(${sortDetails.image})`}}>
                                 </div>
                             </div>
                             <div className={s.main_blog_box}>
                                 <div className={s.main_blog_box_img}>
-                                    <div className={s.main_blog_box_icon}style={{backgroundImage:`url(${card.image})`}}>img</div>
+                                    <div className={s.main_blog_box_icon}style={{backgroundImage:`url(${sortDetails.image})`}}></div>
                                 </div>
                                 <div className={s.main_blog_form}>
-                                    <div className={s.main_blog_title}>{card.title}</div>
-                                    <div className={s.main_blog_date}>Blog creation date: &nbsp;&nbsp;{card.date}</div>
+                                    <div className={s.main_blog_title}>{sortDetails.title}</div>
+                                    <div className={s.main_blog_date}>Blog creation date: &nbsp;&nbsp;{sortDetails.date}</div>
                                     <div className={s.main_blog_web}> Website:
-                                        <a href={`${card.website}`}>{card.website}</a>
+                                        <a href={`${sortDetails.website}`}>{sortDetails.website}</a>
                                     </div>
-                                    <div className={s.main_blog_text}>{card.text}</div>
+                                    <div className={s.main_blog_text}>{sortDetails.text}</div>
                                 </div>
 
 
@@ -39,9 +43,9 @@ const DetailBlock = () => {
                         )
                     }
                     <section className={s.blogs_list}>
-                        {detail.map(d => {
+                        {details.map((d, i) => {
                                 return (
-                                    <article key={d.id} className={s.blogs_list_column} onClick={()=>setSelected(d.id)}>
+                                    <article key={d.id} className={s.blogs_list_column} onClick={()=>onClickListItem(i)}>
                                         <div className={s.blog_box_img} onClick={()=>setIsOpen(!isOpen)}>
                                             <div className={s.blog_cad_img}
                                                           style={{backgroundImage:`url(${d.image})`}}>

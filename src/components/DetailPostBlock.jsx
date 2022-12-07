92eee23a7f0f8f6d75451c3549b6379aacf1af6d
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-12-07 23:31:10 +0300

    changing  DetailPostBlock.jsx

--- src/components/DetailPostBlock.jsx
+++ src/components/DetailPostBlock.jsx
@@ -1,12 +1,10 @@
 import React, {useState} from 'react';
 import s from "../styles/DetailPostBlock.module.css";
-import {initialPostsTasks} from "./Post";
-
 
 
 const DetailPostBlock = ({onClickListItem, details, sortDetails}) => {
-    const [open, setOpen]=useState(false)
-    const selectTitle=['for public posts','for private posts']
+    const [open, setOpen] = useState(false)
+    const selectTitle = ['for public posts', 'for private posts']
     return (
         <div className={s.blogs}>
             <div className={s.box}>
@@ -16,24 +14,28 @@ const DetailPostBlock = ({onClickListItem, details, sortDetails}) => {
                         <p>Back-end blog</p>
                     </div>
                     <div className={s.title}>
-                       <h2>
-                           First day at the office
-                       </h2>
-                        <span>(for public posts)</span>
+                        <h2>
+                            First day at the office
+                        </h2>
+                        <span onClick={()=>setOpen(!open)}>(for public posts)</span>
                     </div>
-                    <div className={s.select_box}>
-                        <ul>
-                            {selectTitle.map((s , i)=>(
-                                <li key={i}> {s}</li>)
-                            )}
+                    {open && (
+                        <div className={s.select_box}>
+                            <ul>
+                                {selectTitle.map((s, i) => (
+                                    <li key={i}> {s}</li>)
+                                )}
 
-                        </ul>
-                    </div>
+                            </ul>
+                        </div>
+                    )
+                    }
+
                     <div className={s.date_info}>
                         12/12/2022 at 15:46:58
                     </div>
                     <div className={s.main_img}>
-                        <img src={sortDetails.image}  alt="img"/>
+                        <img src={sortDetails.image} alt="img"/>
                     </div>
                     <textarea className={s.text_form} name="" id="" cols="30" rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolorum quis repellendus sunt veritatis. Accusamus aperiam architecto blanditiis dolores harum impedit minima non quo saepe totam ullam, vel vero voluptatibus.
                     </textarea>

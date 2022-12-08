d4b631d21e6750f4a6c3b6c070966f4f440b40f0
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-12-08 23:04:16 +0300

    changing  Menu.module.css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,18 +4,24 @@ import s from '../styles/Menu.module.css'
 
 const Menu = ({header, items, active, setActive}) => {
     return (
-        <div className={active ? `${s.menu}${s.active}`: s.menu}>
-            <div className={s.blur}></div>
-            <div className={s.menu_content}>
-                <div className={s.menu_header}>{header}</div>
-                <ul>
-                    {items.map(item=>
-                    <li>
-                        <Link to={item.link}>{item.value}</Link>
-                        <span className={s.icons}>{item.icon}</span>
-                    </li>)}
-                </ul>
+        <div className={active ? s.navigation_active: s.navigation}>
+            <div className={active? s.toggle_active: s.toggle} onClick={()=>setActive(!active)}>
+                <span></span>
             </div>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+            </ul>
+            {/*<div className={s.menu_content}>*/}
+            {/*    <div className={s.menu_header}>{header}</div>*/}
+            {/*    <ul>*/}
+            {/*        {items.map(item=>*/}
+            {/*        <li>*/}
+            {/*            <Link to={item.link}>{item.value}</Link>*/}
+            {/*            <span className={s.icons}>{item.icon}</span>*/}
+            {/*        </li>)}*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
 
 
         </div>

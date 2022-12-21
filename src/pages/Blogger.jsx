698a61e6994cfb551f0545dda6b9f5fb95f67650
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-12-21 23:53:02 +0300

    changing Blogger.jsx

--- src/pages/Blogger.jsx
+++ src/pages/Blogger.jsx
@@ -81,7 +81,7 @@ const Blogger = () => {
                 // onClick={handleTitleChange}
             />
             <Search onChange={handleSearchChange}/>
-            <BlogsList tasks={tasks} setModal={setModal} modal={modal} blog={blog} setBlog={setBlog} onChange={addNewBlog}/>
+            <BlogsList tasks={tasks} setModal={setModal} modal={modal} />
             <SuperModal visible={modal} setVisible={setModal}>
                 <BlogInputForm blog={blog} setBlog={setBlog} onChange={addNewBlog}/>
 

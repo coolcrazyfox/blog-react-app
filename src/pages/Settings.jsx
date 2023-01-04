ab634ed8c4e34912ff90823a72c9187fa1ff1963
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-04 23:48:52 +0300

    changing Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -11,34 +11,44 @@ export const initialPosts = [
     {
         id: 1,
         image: 'https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg',
-        title: 'The best blog in our village',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectet adipiscing elit...',
+        text: 'The best blog in our village',
+        title: "Let's fly into space",
         date: '2022-11-19'
     },
     {
         id: 2,
         image: 'https://st2.depositphotos.com/2001755/5443/i/600/depositphotos_54431143-stock-photo-beautiful-landscape.jpg',
-        title: 'Warriors',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectet adipiscing elit...',
+        text: 'IT industry',
+        title: "I'm sitting at work",
         date: '2022-11-19'
     },
     {
         id: 3,
         image: 'https://static8.depositphotos.com/1017908/938/i/450/depositphotos_9385411-stock-photo-dramatic-clouds-with-mountain-and.jpg',
-        title: 'Audience Platform',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectet adipiscing elit...',
-        date: '2022-11-19'
+        text: 'Blog of beauties',
+        title: "I sell cosmetics",
+        date: '2022-11-20'
     },
     {
         id: 4,
         image: 'https://st2.depositphotos.com/2001755/5967/i/450/depositphotos_59677045-stock-photo-beautiful-landscape.jpg',
-        title: 'Audience Platform',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectet adipiscing elit...sdfgsdgsd    ',
-        date: '2022-11-19'
+        text: 'The best blog in our village',
+        title: "I'm happy like a child",
+        date: '2022-11-22'
+    },
+    {
+        id: 5,
+        image: 'https://picsy.ru/images/photos/1200/1/foto-na-avatarku-priroda-28098386.jpg',
+        text: 'IT industry',
+        title: "Cooking buns",
+        date: '2022-11-22'
+    },
+    {
+        id: 6,
+        image: 'https://bipbap.ru/wp-content/uploads/2017/04/2-8.jpg',
+        text: 'Blog of beauties',
+        title: "First day at the office",
+        date: '2022-11-23'
     },
 ]
 
@@ -102,7 +112,7 @@ const Settings = () => {
                 modal={modal}
                 // blogs={sortedBlogs}
             />
-            <BlogForm create={createNewPost}/>
+
 
         </div>
     );

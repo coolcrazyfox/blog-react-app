f56a390f2a952ef0953bbef4244f4cdbfbab1ef8
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-08 18:08:50 +0300

    changing TestInput.jsx

--- src/components/UI/Test/TestInput.jsx
+++ src/components/UI/Test/TestInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const TestInput = () => {
+const TestInput = ({...props}) => {
     return (
         <>
-            <input type="text"/>
+            <input {...props}/>
 
         </>
     );

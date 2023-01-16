550ec9e43ade369b25a7ca5f3fef074b258f280e
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-16 22:46:53 +0300

    changing a Particle.jsx

--- src/components/UI/Particles/Particle.jsx
+++ src/components/UI/Particles/Particle.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const Particle = () => {
+    const particlesInit = async (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
     return (
-        <div>
-            
-        </div>
+
+            <Particles id="particles-here" init={anInitFunction} options={
+                // an config object
+            } />
+
+
     );
 };
 

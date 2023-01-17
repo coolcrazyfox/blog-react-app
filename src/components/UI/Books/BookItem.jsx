8d22ce6b436af34bbce22c62bfbfef7b30f7f54f
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-17 21:53:26 +0300

    changing BookItem.jsx

--- src/components/UI/Books/BookItem.jsx
+++ src/components/UI/Books/BookItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const BookItem = () => {
+const BookItem = ({book, totalPrice}) => {
     return (
-        <div>
-
+        <div className="book">
+            <div className="book__body">
+                <div className="book__column" onClick={() => totalPrice(book)}>
+                    <div className="book__number">
+                        {book.id}.
+                    </div>
+                    <div className="book__name">
+                        {book.name}
+                    </div>
+                </div>
+                <div className="book__column">
+                    <div className="book__price">
+                        <b>{book.price} $</b>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

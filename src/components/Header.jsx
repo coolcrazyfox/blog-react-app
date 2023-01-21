51f07b3236f45f94a4b84df3f0da8a04df73bc78
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-21 22:54:06 +0300

    changing DetailPostBlock.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,16 @@ import sound from '../assets/message-to-bears-mountains.mp3';
 import Zoom from 'react-reveal/Zoom';
 
 const Header = ({title}) => {
-    const headerTitle='Blogger Platform '
-    const [modalActive, setModalActive]= useState(true)
-    const [isActive, setIsActive]=useState(false)
-    const [play, {stop}]=useSound(sound)
-    const handlePlayMusic=()=>{
+    const headerTitle = 'Blogger Platform '
+    const [modalActive, setModalActive] = useState(true)
+    const [isActive, setIsActive] = useState(false)
+    const [play, {stop}] = useSound(sound)
+    const handlePlayMusic = () => {
         play()
         setIsActive(true)
         setModalActive(true)
     }
-    const handleStopMusic=()=>{
+    const handleStopMusic = () => {
         stop()
         setIsActive(false)
         setModalActive(false)
@@ -24,18 +24,22 @@ const Header = ({title}) => {
     return (
         <div className={s.header}>
             <div className={s.title}>
-                <h3>{headerTitle}</h3>
+                <h3>
+                    <span>
+                        {headerTitle}
+                    </span>
+                </h3>
                 {/*<h4>{headerTitle}</h4>*/}
                 <div className={s.logo}></div>
-                <div className={s.music_box }>
-                    <Zoom >
+                <div className={s.music_box}>
+                    <Zoom>
                         <div className={s.play_icon}>
-                            <SiApplemusic onClick={handlePlayMusic}  className={!isActive ? s.music: s.music_active}/>
+                            <SiApplemusic onClick={handlePlayMusic} className={!isActive ? s.music : s.music_active}/>
                             <span className={s.span}>Play </span>
 
                         </div>
-                        <div className={isActive ? s.stop_icon: `${s.stop_icon}${s.active}`}>
-                            <TbMusicOff onClick={handleStopMusic } />
+                        <div className={isActive ? s.stop_icon : `${s.stop_icon}${s.active}`}>
+                            <TbMusicOff onClick={handleStopMusic}/>
                             <span className={s.span_second}>Stop </span>
                         </div>
                     </Zoom>

945c01f2e74dbf4597fd3e9352ebb4cd9e6135ca
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2023-01-23 23:10:02 +0300

    changing Preloader.module.css

--- src/components/UI/Test/TestsList.jsx
+++ src/components/UI/Test/TestsList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Test from "./Test";
 import TestButton from "./TestButton";
+import {MdCreateNewFolder} from "react-icons/md";
 
 const TestsList = ({posts, onClick}) => {
     return (
         <div style={{margin:'10px'}}>
-            <TestButton onClick={onClick}>Create</TestButton>
+            <TestButton onClick={onClick}>
+                <MdCreateNewFolder/>
+                <br/><span>create</span>
+            </TestButton>
             {posts.map(post=>(
                 <Test post={post} key={post.id}/>)
             )}

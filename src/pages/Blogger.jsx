2ba879bcdf87dc38d2c68060ca798193ec9c76df
Andrei Malets <coolviktori@yandex.by>
2023-02-03 22:48:12 +0300

    changing theme

--- src/pages/Blogger.jsx
+++ src/pages/Blogger.jsx
@@ -52,7 +52,7 @@ export const ThemeContext= createContext(null)
 
 const Blogger = () => {
     //theme
-    const [theme, setTheme]= useState('light')
+    const [theme, setTheme]= useState('dark')
     const toggleTheme=()=>{
         setTheme((curr)=>(curr ==='light' ? 'dark' : 'light'))
     }

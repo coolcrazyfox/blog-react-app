6151f7d6b03766012132f11876cd5a8924bf99df
Andrei Malets <coolviktori@yandex.by>
2023-02-05 23:29:55 +0300

    service

---
+++ src/API/PostService.js
@@ -0,0 +1,13 @@
+
+export default class PostService {
+    static async getAll(){
+        try{
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        return response.data
+        }
+        catch(e){
+            console.log(e)
+        }
+        
+    }
+}

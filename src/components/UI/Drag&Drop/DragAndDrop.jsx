c7de063f708502360d96c5b42e92d3b3c5b30a87
Andrei Malets <coolviktori@yandex.by>
2023-02-07 22:21:51 +0300

    changes DR&Dr

--- src/components/UI/Drag&Drop/DragAndDrop.jsx
+++ src/components/UI/Drag&Drop/DragAndDrop.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const DragAndDrop = () => {
-    const [] = useState()
+    const [cardList, setCardList] =React.useState(
+        {id: 1, order: 2, text: 'card #2'},
+        {id: 2, order: 1, text: 'card #1'},
+        {id: 3, order: 4, text: 'card #4'},
+        {id: 4, order: 3, text: 'card #3'}
+        )
   return (
     <div>
       

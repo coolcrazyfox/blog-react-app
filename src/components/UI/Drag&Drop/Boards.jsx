f3a3548deb520f8a5bd5143a251f0ad4d99fbf54
Andrei Malets <coolviktori@yandex.by>
2023-02-08 23:27:26 +0300

    changes boards

--- src/components/UI/Drag&Drop/Boards.jsx
+++ src/components/UI/Drag&Drop/Boards.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Boards = () => {
     const [boards, setBoards]= useState([
-        {},
-        {},
-        {}
+        {id: 1, title: 'To do', items: [{id: 1, title: 'Go to shop'}, {id: 2, title: 'Buy a SSD'}] },
+        {id: 2, title: 'Check on', items: [{id: 3, title: 'Go to shop'}, {id: 4, title: 'Buy a SSD'}]},
+        {id: 3, title: 'Made', items: [{id: 5, title: 'Go to shop'}, {id: 6, title: 'Buy a SSD'}]}
     ])
   return (
     <div>

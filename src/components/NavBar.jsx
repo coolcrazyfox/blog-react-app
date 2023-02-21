2a7bc66444b9ff5670a93d327d7166a371fd1892
Andrei Malets <coolviktori@yandex.by>
2023-02-21 22:33:28 +0300

    changing style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,8 +11,8 @@ import Fade from "react-reveal/Fade";
 
 export const initialState = [
     {id: 1, link: '/', title: "Home", color: "#D73486FF", icon: <SiHomeassistant/>},
-    {id: 2, link: '/blogger', title: "Blogs", color: "#cea602", icon: <SiMicrodotblog/>},
-    {id: 3, link: '/posts', title: "Posts", color: "#864E4CFF", icon: <SiApostrophe/>},
+    {id: 2, link: '/blogger', title: "Blogs", color: "#dbc711", icon: <SiMicrodotblog/>},
+    {id: 3, link: '/posts', title: "Posts", color: "#6a99ff", icon: <SiApostrophe/>},
     {id: 4, link: '/users', title: "Users", color: "#8BB92CFF", icon: <HiUserGroup/>},
     {id: 5, link: '/registration', title: "Log In", color: "#ec8434", icon: <RiLoginCircleFill/>},
     {id: 6, link: '/settings', title: "Settings", color: "#A61111FF", icon: <MdSettingsSuggest/>}

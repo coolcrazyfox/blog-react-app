cefdee87f073a86909eb42eac3ef9631a6313833
Andrei Malets <coolviktori@yandex.by>
2023-02-22 22:35:27 +0300

    test

--- src/pages/Blogger.jsx
+++ src/pages/Blogger.jsx
@@ -21,33 +21,33 @@ export const initialTasks = [
         id: 1,
         image: 'https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg',
         title: 'The best blog in our village',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-        date: '2022-11-20'
+        websiteUrl: 'https://www.youtube.com/',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
+        createdAt: '2022-11-20'
     },
     {
         id: 2,
         image: 'https://st2.depositphotos.com/2001755/5443/i/600/depositphotos_54431143-stock-photo-beautiful-landscape.jpg',
         title: 'Warriors',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-        date: '2022-11-19'
+        websiteUrl: 'https://www.youtube.com/',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
+        createdAt: '2022-11-19'
     },
     {
         id: 3,
         image: 'https://static8.depositphotos.com/1017908/938/i/450/depositphotos_9385411-stock-photo-dramatic-clouds-with-mountain-and.jpg',
         title: 'Audience Platform',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-        date: '2022-11-21'
+        websiteUrl: 'https://www.youtube.com/',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
+        createdAt: '2022-11-21'
     },
     {
         id: 4,
         image: 'https://st2.depositphotos.com/2001755/5967/i/450/depositphotos_59677045-stock-photo-beautiful-landscape.jpg',
-        title: 'Audience Platform',
-        website: 'https://www.youtube.com/',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-        date: '2022-11-20'
+        name: 'Audience Platform',
+        websiteUrl: 'https://www.youtube.com/',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
+        createdAt: '2022-11-20'
     },
 ]
 const themesB = themes
@@ -65,9 +65,9 @@ const Blogger = () => {
 
     //select
     const selectValue = [
-        {value: 'title', name: 'New blogs first'},
+        {value: 'name', name: 'New blogs first'},
         {value: 'text', name: 'Old blogs first'},
-        {value: 'title', name: 'From A to Z'},
+        {value: 'name', name: 'From A to Z'},
         {value: 'text', name: 'From Z to A'},
 
     ]
